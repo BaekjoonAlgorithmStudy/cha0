429b2a16dd88c64923301eeb3320d347777d595d
chayounglee <chayounglee@gmail.com>
2017-11-28 18:27:57 +0900

    5 problems committed

---
+++ cha0/src/io/AnswerNoId.java
@@ -0,0 +1,9 @@
+package io;
+
+public class AnswerNoId {
+	public static void main(String[] args) {
+		System.out.println(3);
+		System.out.println("kiwibird");
+	}
+
+}
---
+++ cha0/src/io/Dog.java
@@ -0,0 +1,8 @@
+package io;
+
+public class Dog {
+	public static void main(String[] args) {
+		System.out.println("|\\_/|\n|q p|\t/}\n( 0 )\"\"\"\\\n|\"^\"'\t |\n||_/=\\\\__|");		
+	}
+
+}
