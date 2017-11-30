90208d6a21220e511e254eab5c7411b9e37c8968
chayounglee <chayounglee@gmail.com>
2017-11-30 12:13:04 +0900

    Remainder updated and committed

--- cha0/src/two/Remainder.java
+++ cha0/src/two/Remainder.java
@@ -1,12 +1,26 @@
 package two;
 
+import java.util.Scanner;
+
 public class Remainder {
 	public static void main(String[] args) {
-		/*
-		 * (A+B)%C는 (A%C + B%C)%C 와 같을까?
-		 * (A×B)%C는 (A%C × B%C)%C 와 같을까?
-		 * 세 수 A, B, C가 주어졌을 때, 위의 네가지 값을 구하는 프로그램을 작성하시오.
-		 */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		int[] results = {(A+B)%C, (A%C + B%C)%C, (A*B)%C, (A%C * B%C)%C};
+		
+		for(int tmp:results) {
+			System.out.println(tmp);
+		}
+		
+		
+		
+		
+		
 	}
 
 }
